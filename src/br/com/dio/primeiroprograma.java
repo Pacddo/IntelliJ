2874fad28988d8f14dd9b2f43aa6fe3400d0a83e
pacddo <douglas.pacheco@hotmail.com>
2022-02-23 10:22:45 -0300

    Cria primeiro programa

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 4;
+        System.out.println("Hello World" + (a+b));
+
+
+    }
+}
